7cef51433702492cd5c5f9d0773afcdcf95be203
Oyiga victor <victoroyiga01@gmail.com>
2023-12-27 16:42:47 +0100

    update

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,7 +4,6 @@ import { useNavigate } from "react-router-dom";
 export default function Home() {
   const [RoomCode, setRoomCode] = useState();
 
-  console.log(RoomCode);
 
   const navigate = useNavigate();
 
